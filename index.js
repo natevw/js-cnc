ba486e2cdf7d894a95f11daa56495595dd55c822
Nathan Vander Wilt <nate@calftrail.com>
2015-02-18 22:35:55 -0800

    is alive! add socket connection between browser and cnc, use for status and super basic jog

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ var serialport = require('serialport'),
 
 // TODO: https://github.com/grbl/grbl/wiki/Interfacing-with-Grbl
 
+var cnc;
 new serialport.SerialPort("/dev/tty.usbmodem621", {
   baudrate: 115200,
   parser: serialport.parsers.readline("\r\n")
 }).on('open', function () {
   var port = this;
   console.log("opened");
-  var cnc = new Grbl(port);
+  cnc = new Grbl(port);
   cnc.on('ready', function () {
-    setInterval(function () {
-      cnc.write("G91 G1 F1 X1", function (e) {
-        console.log("EXCHANGED", e);
-      });
-    }, 100);
+//    setInterval(function () {
+//      cnc.write("G91 G1 F1 X1", function (e) {
+//        console.log("EXCHANGED", e);
+//      });
+//    }, 5e3);
     setInterval(function () {
       cnc.request('status');
     }, 1e3);
@@ -27,4 +28,33 @@ new serialport.SerialPort("/dev/tty.usbmodem621", {
   throw e;
 });
 
-//require('repl').start({});
+
+var WebSocket = require('faye-websocket'),
+    http      = require('http');
+http.createServer(function (req, res) {
+  // TODO: host static files
+}).on('upgrade', function (req, sock, body) {
+  if (WebSocket.isWebSocket(req)) {
+    var ws = new WebSocket(req, sock, body);
+    function sendJSON(d) {
+      ws.send(JSON.stringify(d));
+    }
+    console.log("ws opened");
+    ws.on('message', function (evt) {
+      console.log("ws data:", evt.data);
+      cnc.write(evt.data, function (e) {
+        if (e) sendJSON({error:e.message});
+        else sendJSON({ok:true});
+      });
+    });
+    cnc.on('status', function (arr) {
+      sendJSON({status:arr});
+    });
+    
+    ws.on('close', function (evt) {
+      console.log("ws closed", evt.code, evt.reason);
+    });
+  }
+}).listen(8000, function () {
+  console.log("listening on port", this.address().port);
+});
